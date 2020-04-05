4bcfdb3e515e5aa8f34e56461f0cd2b40f79f648
g-harel <gabrielj.harel@gmail.com>
2020-04-04 23:22:43 -0500

    include bundle type definitions alongside bundles

--- rollup.config.js
+++ rollup.config.js
@@ -1,31 +1,43 @@
 import typescript from "rollup-plugin-typescript2";
 import {uglify} from "rollup-plugin-uglify";
+import copy from "rollup-plugin-copy";
 
 const bundles = [
     {
         name: "blobs",
         entry: "public/legacy.ts",
-        output: "index.js",
+        types: "public/legacy.d.ts",
+        output: ".",
     },
     {
         name: "blobs",
         entry: "public/legacy.ts",
-        output: "v1/index.js",
+        types: "public/legacy.d.ts",
+        output: "v1",
     },
     {
         name: "blobs2",
         entry: "public/blobs.ts",
-        output: "v2/index.js",
+        types: "public/blobs.d.ts",
+        output: "v2",
     },
 ];
 
 export default bundles.map((bundle) => ({
     input: bundle.entry,
     output: {
-        file: bundle.output,
+        file: bundle.output + "/index.js",
         format: "umd",
         name: bundle.name,
         sourcemap: true,
     },
-    plugins: [typescript({cacheRoot: "./node_modules/.cache/rpt2"}), uglify()],
+    plugins: [
+        typescript({cacheRoot: "./node_modules/.cache/rpt2"}),
+        uglify(),
+        copy({
+            hook: "writeBundle",
+            targets: [{src: bundle.types, dest: bundle.output, rename: "index.d.ts"}],
+            verbose: true,
+        }),
+    ],
 }));
