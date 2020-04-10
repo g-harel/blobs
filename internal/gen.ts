9d2b36061c1979465a9b6a7e0a2848849098f944
g-harel <gabrielj.harel@gmail.com>
2020-04-09 21:20:01 -0500

    move new blob generation to shared location to reuse for animation code

--- internal/gen.ts
+++ internal/gen.ts
@@ -1,3 +1,6 @@
+import {rand} from "../internal/rand";
+import {mapPoints} from "../internal/util";
+import {BlobOptions} from "../public/blobs";
 import {Point} from "./types";
 import {smooth} from "./util";
 
@@ -21,4 +24,45 @@ export const genBlob = (pointCount: number, offset: () => number): Point[] => {
     const smoothingStrength = ((4 / 3) * Math.tan(angle / 4)) / Math.sin(angle / 2) / 2;
 
     return smooth(points, smoothingStrength);
+};
+
+const typeCheck = (name: string, val: any, expected: string[]) => {
+    const actual = typeof val;
+    if (!expected.includes(actual)) {
+        throw `(blobs2) "${name}" should have type "${expected.join("|")}" but was "${actual}".`;
+    }
+};
+
+export const genFromOptions = (blobOptions: BlobOptions): Point[] => {
+    const rgen = rand(String(blobOptions.seed));
+
+    typeCheck("blobOptions", blobOptions, ["object"]);
+    typeCheck("seed", blobOptions.seed, ["string", "number"]);
+    typeCheck("extraPoints", blobOptions.extraPoints, ["number"]);
+    typeCheck("randomness", blobOptions.randomness, ["number"]);
+    typeCheck("size", blobOptions.size, ["number"]);
+
+    // Scale of random movement increases as randomness approaches infinity.
+    // randomness = 0   -> rangeStart = 1
+    // randomness = 2   -> rangeStart = 0.8333
+    // randomness = 5   -> rangeStart = 0.6667
+    // randomness = 10  -> rangeStart = 0.5
+    // randomness = 20  -> rangeStart = 0.3333
+    // randomness = 50  -> rangeStart = 0.1667
+    // randomness = 100 -> rangeStart = 0.0909
+    const rangeStart = 1 / (1 + Math.abs(blobOptions.randomness) / 10);
+
+    const points = genBlob(
+        3 + Math.abs(blobOptions.extraPoints),
+        () => (rangeStart + rgen() * (1 - rangeStart)) / 2,
+    );
+
+    const size = Math.abs(blobOptions.size);
+    return mapPoints(points, ({curr}) => {
+        curr.x *= size;
+        curr.y *= size;
+        curr.handleIn.length *= size;
+        curr.handleOut.length *= size;
+        return curr;
+    });
 };
--- public/blobs.ts
+++ public/blobs.ts
@@ -1,9 +1,7 @@
-import {genBlob} from "../internal/gen";
-import {rand} from "../internal/rand";
-import {Point} from "../internal/types";
-import {mapPoints} from "../internal/util";
+import {genFromOptions} from "../internal/gen";
 import {renderPath} from "../internal/render/svg";
 import {renderPath2D} from "../internal/render/canvas";
+import {mapPoints} from "../internal/util";
 
 export interface BlobOptions {
     seed: string | number;
@@ -23,50 +21,9 @@ export interface SvgOptions {
     strokeWidth?: number;
 }
 
-const typeCheck = (name: string, val: any, expected: string[]) => {
-    const actual = typeof val;
-    if (!expected.includes(actual)) {
-        throw `(blobs2) "${name}" should have type "${expected.join("|")}" but was "${actual}".`;
-    }
-};
-
-const raw = (blobOptions: BlobOptions): Point[] => {
-    const rgen = rand(String(blobOptions.seed));
-
-    typeCheck("blobOptions", blobOptions, ["object"]);
-    typeCheck("seed", blobOptions.seed, ["string", "number"]);
-    typeCheck("extraPoints", blobOptions.extraPoints, ["number"]);
-    typeCheck("randomness", blobOptions.randomness, ["number"]);
-    typeCheck("size", blobOptions.size, ["number"]);
-
-    // Scale of random movement increases as randomness approaches infinity.
-    // randomness = 0   -> rangeStart = 1
-    // randomness = 2   -> rangeStart = 0.8333
-    // randomness = 5   -> rangeStart = 0.6667
-    // randomness = 10  -> rangeStart = 0.5
-    // randomness = 20  -> rangeStart = 0.3333
-    // randomness = 50  -> rangeStart = 0.1667
-    // randomness = 100 -> rangeStart = 0.0909
-    const rangeStart = 1 / (1 + Math.abs(blobOptions.randomness) / 10);
-
-    const points = genBlob(
-        3 + Math.abs(blobOptions.extraPoints),
-        () => (rangeStart + rgen() * (1 - rangeStart)) / 2,
-    );
-
-    const size = Math.abs(blobOptions.size);
-    return mapPoints(points, ({curr}) => {
-        curr.x *= size;
-        curr.y *= size;
-        curr.handleIn.length *= size;
-        curr.handleOut.length *= size;
-        return curr;
-    });
-};
-
 export const canvasPath = (blobOptions: BlobOptions, canvasOptions: CanvasOptions = {}): Path2D => {
     return renderPath2D(
-        mapPoints(raw(blobOptions), ({curr}) => {
+        mapPoints(genFromOptions(blobOptions), ({curr}) => {
             curr.x += canvasOptions.offsetX || 0;
             curr.y += canvasOptions.offsetY || 0;
             return curr;
@@ -87,5 +44,5 @@ export const svg = (blobOptions: BlobOptions, svgOptions: SvgOptions = {}): stri
 };
 
 export const svgPath = (blobOptions: BlobOptions): string => {
-    return renderPath(raw(blobOptions));
+    return renderPath(genFromOptions(blobOptions));
 };
