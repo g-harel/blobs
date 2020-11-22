a3f3e7700cad0ea8214d996b01bcb2a93effb232
g-harel <gabrielj.harel@gmail.com>
2020-11-21 21:35:54 -0600

    draw smooth and pixel circles in different cells

--- demo/script.ts
+++ demo/script.ts
@@ -1,5 +1,7 @@
 import {newRow, CellPainter} from "./painter";
 
+// TODO debug flag.
+
 // content
 //     raster vs pixel
 //     bezier curves
@@ -11,15 +13,18 @@ import {newRow, CellPainter} from "./painter";
 //     shape morphing
 //         path splitting
 
-const resetStyles = (ctx: CanvasRenderingContext2D) => {
-    ctx.fillStyle = "black";
-    ctx.setTransform(1,0,0,1,0,0);
+const rotateAround = (
+    options: {ctx: CanvasRenderingContext2D; angle: number; cx: number; cy: number},
+    fn: () => void,
+) => {
+    options.ctx.translate(options.cx, options.cy);
+    options.ctx.rotate(options.angle);
+    fn();
+    options.ctx.setTransform(1, 0, 0, 1, 0, 0);
 };
 
 const gridPainter = (slices: number, a: string, b: string): CellPainter => {
     return (ctx, width, height) => {
-        resetStyles(ctx);
-
         const w = width / slices;
         const h = height / slices;
         for (let i = 0; i < slices; i++) {
@@ -37,12 +42,11 @@ const gridPainter = (slices: number, a: string, b: string): CellPainter => {
 
 const textPainter = (text: string, angle: number): CellPainter => {
     return (ctx, width, height) => {
-        resetStyles(ctx);
-
         const fontSize = width * 0.04;
         const lineHeight = fontSize * 1.3;
         ctx.font = `${fontSize}px -apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Open Sans,Helvetica Neue,sans-serif`;
 
+        // Break up text into lines.
         const lines: string[] = [];
         const lineWidth = width * 0.8;
         const words = text.trim().split(" ");
@@ -59,18 +63,16 @@ const textPainter = (text: string, angle: number): CellPainter => {
         }
         lines.push(currentLine);
 
-        // Rotate text around center of cell.
-        const centerX = 0 || width / 2;
-        const centerY = 0 || height / 2;
-        const startX = (lineWidth) / 2;
-        const startY = (lineHeight * lines.length) / 2;
-        ctx.translate(centerX, centerY);
-        ctx.rotate(angle);
-        for (let i = 0; i < lines.length; i++) {
-            ctx.fillText(lines[i], -startX, -startY + lineHeight * (i + 1));
-        }
-        ctx.translate(-centerX, -centerY);
-        ctx.setTransform(1,0,0,1,0,0);
+        // Draw lines.
+        const cx = width / 2;
+        const cy = height / 2;
+        rotateAround({ctx, cx, cy, angle}, () => {
+            const x = lineWidth / 2;
+            const y = (lineHeight * lines.length) / 2;
+            for (let i = 0; i < lines.length; i++) {
+                ctx.fillText(lines[i], -x, -y + lineHeight * (i + 1));
+            }
+        });
     };
 };
 
@@ -82,38 +84,42 @@ newRow(
     ),
 );
 
-newRow(2, (ctx, width, height) => {
-    const shapeSize = width * 0.4;
+newRow(
+    1,
+    // Pixelated circle.
+    (ctx, width, height) => {
+        const angle = Math.PI / 16;
+        const pt = width * 0.04;
+        const quadrant = [0, 1, 2, 3, 4, 5, 6, 7, 7, 8, 8, 9, 9, 9];
+        const cx = width / 2;
+        const cy = height / 2;
 
-    // Draw pixelated circle.
-    const posX = width * 0.15;
-    const posY = height * -0.1;
-    const cx = posX + shapeSize / 2;
-    const cy = posY + shapeSize / 2;
-    const quadrant = [0, 1, 2, 3, 4, 5, 6, 7, 7, 8, 8, 9, 9, 9];
-    const gridCount = 2 * quadrant.length;
-    const pixels = shapeSize / gridCount;
-    ctx.rotate(Math.PI / 16);
-    for (let i = 0; i < quadrant.length; i++) {
-        const gridX = quadrant[i];
-        const gridY = quadrant[quadrant.length - 1 - i];
-        ctx.fillStyle = "#ec576b";
-        ctx.fillRect(cx + gridX * pixels, cy + gridY * pixels, pixels + 1, pixels + 1);
-        ctx.fillRect(cx - (gridX + 1) * pixels, cy + gridY * pixels, pixels + 1, pixels + 1);
-        ctx.fillRect(cx + gridX * pixels, cy - (gridY + 1) * pixels, pixels + 1, pixels + 1);
-        ctx.fillRect(cx - (gridX + 1) * pixels, cy - (gridY + 1) * pixels, pixels + 1, pixels + 1);
-    }
+        rotateAround({ctx, cx, cy, angle}, () => {
+            for (let i = 0; i < quadrant.length; i++) {
+                const gridX = quadrant[i];
+                const gridY = quadrant[quadrant.length - 1 - i];
+                ctx.fillStyle = "#ec576b";
+                ctx.fillRect(gridX * pt, gridY * pt, pt + 1, pt + 1);
+                ctx.fillRect(-(gridX + 1) * pt, gridY * pt, pt + 1, pt + 1);
+                ctx.fillRect(gridX * pt, -(gridY + 1) * pt, pt + 1, pt + 1);
+                ctx.fillRect(-(gridX + 1) * pt, -(gridY + 1) * pt, pt + 1, pt + 1);
+            }
+        });
+    },
+    // Smooth circle.
+    (ctx, width, height) => {
+        const pt = width * 0.04;
+        const shapeSize = width * 0.8;
+        const cx = width / 2;
+        const cy = height / 2;
 
-    // Reset rotation.
-    ctx.setTransform(1, 0, 0, 1, 0, 0);
-
-    // Draw smooth circle.
-    ctx.beginPath();
-    ctx.arc(width * 0.7, height * 0.1 + shapeSize / 3, shapeSize / 3, 0, 2 * Math.PI);
-    ctx.lineWidth = pixels;
-    ctx.strokeStyle = "#ec576b";
-    ctx.stroke();
-});
+        ctx.beginPath();
+        ctx.arc(cx, cy, shapeSize / 2, 0, 2 * Math.PI);
+        ctx.lineWidth = pt;
+        ctx.strokeStyle = "#ec576b";
+        ctx.stroke();
+    },
+);
 
 newRow(2, gridPainter(4, "#ec576b", "white"), gridPainter(80, "#ec576b", "white"));
 newRow(2, gridPainter(16, "#ec576b", "white"));
